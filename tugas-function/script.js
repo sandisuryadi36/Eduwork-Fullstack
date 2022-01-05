7133a3797fdf492c018b316d5778ba5daa4949b9
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-01-05 23:38:42 +0700

    finish tugas function

--- tugas-function/script.js
+++ tugas-function/script.js
@@ -1,4 +1,7 @@
 function playerInput() {
+    alert(`Ronde ${nowRonde}`)
+    let a = prompt("Player 1\nMasukkan tebakan anda: ")
+    let b = prompt("Player 2\nMasukkan tebakan anda: ")
     return {
         playerOne: a,
         playerTwo: b,
@@ -6,15 +9,53 @@ function playerInput() {
 }
 
 function validateInput(a, b) {
+    if (/[a-z]|[A-Z]/.test(a + b)) {    // cek apakah angka atau bukan
+        return "Mohon masukkan angka!"
+    } else if (!(a > 0 && a <= maxAngka) | !(b > 0 && b <= maxAngka)) {     // cek apakah input sesuai dengan range
+        return `Masukkan angka antara ${minAngka} dan ${maxAngka}!`
+    } else if (a == b) {                // cek apakah input sama
+        return "Tebakan tidah boleh sama!"
+    }
     return ""
 }
 
 function checkAnswer(x, a, b) {
     // cek ada pemenang atau tidak
-    // printout hasil
     // tambahkan score jika ada pemenang
-    // tambahkan nowRonde jika ada pemenang
+    // tambahkan nowRonde
+    // printout hasil
 
+    if (x == a) {
+        playerOneScore++
+        nowRonde++
+        alert(`
+Player 1 WIN!
+-------------
+angka tebakan adalah: ${x}
+tebakan Player 1: ${a}
+tebakan Player 2: ${b}`
+        )
+    } else if (x == b) {
+        playerTwoScore++
+        nowRonde++
+        alert(`
+Player 2 WIN!
+-------------
+angka tebakan adalah: ${x}
+tebakan Player 1: ${a}
+tebakan Player 2: ${b}`
+        )
+    } else {
+        nowRonde++
+        alert(`
+Permainan Seri!
+Tidak ada yang benar
+-------------
+angka tebakan adalah: ${x}
+tebakan Player 1: ${a}
+tebakan Player 2: ${b}`
+        )
+    }
 }
 
 
@@ -31,28 +72,37 @@ Selamat bermain!!`)
 let nowRonde = 1
 let playerOneScore = 0
 let playerTwoScore = 0
-while (nowRonde <= 5) {
+let winner = ""
+while (nowRonde <= ronde) {
     do {
         var input = playerInput()
         var validation = validateInput(input.playerOne, input.playerTwo)
-        if (validation != "") {
+        if (validation != "") {         // keluarkan pesan jika ada error
             alert(validation)
             alert("ulangi memasukkan tebakan!")
         }
-    } while (validation != "")
-    let x = randomNuber()
+    } while (validation != "")          // cek apakah ada error atau tidak, jika ada ulangi
+    let x = Math.floor(Math.random() * maxAngka) + minAngka
     checkAnswer(x, input.playerOne, input.playerTwo)
 }
-let winner = () => {
-    if (playerOneScore > playerTwoScore) {
-        return "Player 1"
-    } else {
-        return "Player 2"
-    }
-}
 
-alert(`::: GAME OVER :::
-Pemenangnya adalah ${winner}
+// print out hasil akhir
+if (playerOneScore != playerTwoScore) {
+    if (playerOneScore > playerTwoScore) {
+        winner = "Player 1"
+    } else {
+        winner = "Player 2"
+    }
+
+    alert(`::: GAME OVER :::
+Pemenangnya adalah [${winner}]
 Score:
 Player 1 : ${playerOneScore}
 Player 2 : ${playerTwoScore}`)
+} else {
+    alert(`::: GAME OVER :::
+Permainan seri!
+Score:
+Player 1 : ${playerOneScore}
+Player 2 : ${playerTwoScore}`)
+}
