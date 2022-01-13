7e0c6de7d9f419d432407b288f0be1392bc6ded7
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-01-14 01:17:00 +0700

    add pagination feature and category menu

--- tugas-promise/src/content.js
+++ tugas-promise/src/content.js
@@ -1,11 +1,18 @@
 class loadContent {
-    constructor(lang, category, q) {
-        this.token = "5be7d56373774432b6f59713eebbfdba"
+    constructor(lang, category, q, page) {
+        // this.token = "5be7d56373774432b6f59713eebbfdba"
+        this.token = "48fc76a73f9342b690e5a4bed5e4d1c7"
+        this.page = page
         if (q != "") {
-            this.url = `https://newsapi.org/v2/everything?q=${q}&language=${lang}&apiKey=${this.token}`
-            this.header = `Shearch result for "${q}"`
+            if (category != "general") {
+                this.url = `https://newsapi.org/v2/top-headlines?q=${q}&language=${lang}&category=${category}&page=${page}&apiKey=${this.token}`
+                this.header = `Shearch result for "${q}" in ${category.charAt(0).toUpperCase() + category.slice(1)}`
+            } else {
+                this.url = `https://newsapi.org/v2/everything?q=${q}&language=${lang}&page=${page}&apiKey=${this.token}`
+                this.header = `Shearch result for "${q}"`
+            }
         } else {
-            this.url = `https://newsapi.org/v2/top-headlines?language=${lang}&category=${category}&apiKey=${this.token}`
+            this.url = `https://newsapi.org/v2/top-headlines?language=${lang}&category=${category}&page=${page}&apiKey=${this.token}`
             if (category == "general") {
                 this.header = "Headline News"
             } else {
@@ -51,7 +58,72 @@ class loadContent {
                 dataJSON.articles.forEach(article => {
                     cards.push(article)
                 })
-                result = this.loadCards(cards)
+
+                function pageView(page) {
+                    let view = ""
+                    if (page != 1) {
+                        view += `
+                        <li class="page-item" onclick="prevPage(${page-1})">
+                            <a class="page-link" href="#">Previous</a>
+                        </li>
+                        `
+                    } else {
+                        view += `
+                        <li class="page-item disabled">
+                                <a class="page-link">Previous</a>
+                            </li>
+                        `
+                    }
+
+                    let s
+                    if (page - 2 <= 1) {
+                        s = 1
+                    } else s = page - 2
+                    for (let i = s; i < page; i++){
+                        view += `
+                        <li class="page-item" onclick="prevPage(${i})"><a class="page-link" href="#">${i}</a></li>`
+                    }
+
+                    view += `<li class="page-item active"><a class="page-link">${page}</a></li>`
+
+                    for (let i = page; i < page + 2; i++){
+                        if ((dataJSON.totalResults - (i * 20)) > 0) {
+                            view += `<li class="page-item"><a class="page-link" href="#" onclick="nextPage(${i+1})">${i+1}</a></li>`
+                        }
+                    }
+                    if ((dataJSON.totalResults - (page*20)) > 0) {
+                        view += `
+                            <li class="page-item" onclick="nextPage(${page+1})">
+                                <a class="page-link" href="#">Next</a>
+                            </li>
+                            `
+                    } else {
+                        view += `
+                        <li class="page-item disabled">
+                                <a class="page-link">Next</a>
+                            </li>
+                        `
+                    }
+                    return view
+                }
+
+                function pagination(page) {
+                    let view = `
+                    <nav aria-label="Page navigation" class="container" id="pagination">
+                        <ul class="pagination pagination-sm justify-content-end">
+                            ${pageView(page)}
+                        </ul>
+                    </nav>
+                    `
+                    return view
+                }
+
+                result = pagination(this.page)
+                result += `<div class="row row-cols-lg-3 row-cols-md-2 row-cols-1 g-3">`
+                result += this.loadCards(cards)
+                result += `</div>`
+                result += pagination(this.page)
+
             } else if (dataJSON.status == "ok") {
                 result = `<p>No result found</p>`
             } else if (dataJSON.status == "error") {
@@ -60,9 +132,7 @@ class loadContent {
 
             let content =
                 `<h1 class="my-3">${this.header}</h1>` +
-                `<div class="row row-cols-lg-3 row-cols-md-2 row-cols-1 g-3">` +
-                result +
-                `</div>`
+                result
             element.innerHTML = content
         })
             .catch(err => {
--- tugas-promise/src/index.js
+++ tugas-promise/src/index.js
@@ -4,33 +4,66 @@ const content = document.getElementById("content")
 let lang = "id"
 let category = "general"
 let searchQuery = ""
+let page = 1
 
 // render home view
+goHome()
+
 function goHome() {
-    const home = new loadContent(lang, category, searchQuery)
+    const home = new loadContent(lang, category, searchQuery, page)
     home.render(content)
 }
-goHome()
+
+// view vontent
+function viewContent(lang, category, searchQuery, page) {
+    const view = new loadContent(lang, category, searchQuery, page)
+    view.render(content)
+}
 
 // search script
 function searchNow() {
     searchQuery = document.getElementById("search-bar").value
-    const search = new loadContent(lang, category, searchQuery)
-    search.render(content)
+    page = 1
+    viewContent(lang, category, searchQuery, page)
 }
 
-document.getElementById("search-btn").addEventListener("click", searchNow)
+function timeOutInput() {
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+        searchNow()
+    }, 500)
+}
+
+// document.getElementById("search-btn").addEventListener("click", searchNow)
 
 let timer
 document.getElementById("search-bar").addEventListener("focus", function () {
     if (document.getElementById("live-search-check").checked) {
-        document.getElementById("search-bar").addEventListener("keyup", function () {
-            clearTimeout(timer);
-            timer = setTimeout(function () {
-                searchNow()
-            }, 1000)
-        })
+        document.getElementById("search-bar").addEventListener("input", timeOutInput)
     } else {
-        document.getElementById("search-bar").removeEventListener("keyup", function(){})
+        document.getElementById("search-bar").removeEventListener("input", timeOutInput)
     }
 })
+
+// pagination script
+function prevPage(x) {
+    page = x
+    viewContent(lang, category, searchQuery, page)
+}
+function nextPage(x) {
+    page = x
+    viewContent(lang, category, searchQuery, page)
+}
+
+function viewCategory(categoryInput) {
+    category = categoryInput
+    document.getElementById("search-bar").value=""
+    searchQuery = ""
+    page = 1
+    viewContent(lang, category, searchQuery, page)
+}
+
+window.searchNow= searchNow
+window.prevPage = prevPage
+window.nextPage = nextPage
+window.viewCategory = viewCategory
